df725d0c900549880e98a816b918aad00b0774eb
gwprat <gwprat@ensc.fr>
2024-01-30 13:39:53 +0100

    correction image rocher lag

--- GameManager/screen_maker.py
+++ GameManager/screen_maker.py
@@ -8,7 +8,7 @@ screen = pygame.display.set_mode((window_width, window_heigth))
 def draw_world():
     for row in WORLD_OBJECTS:
         for elem in row:
-            screen.blit(Tile(elem.hitbox.y,elem.hitbox.x,'.').sprite,(elem.hitbox.y,elem.hitbox.x)) #Affiche toujours de l'herbe en fond
+            #screen.blit(Tile(elem.hitbox.y,elem.hitbox.x,'.').sprite,(elem.hitbox.y,elem.hitbox.x)) #Affiche toujours de l'herbe en fond
             screen.blit(elem.sprite,(elem.hitbox.y,elem.hitbox.x)) #affiche l'élément de premier plan du niveau à la case donnée 
          
 def draw_character(character):
